869806c1f00a41d429dcf922bc45d9caf8a5f7ab
Edson Cifuentes <eddacigo.208@gmail.com>
2025-11-15 16:02:50 -0500

    Update find_runner_up_score.py
    Commented for context

--- hackerrank/find_runner_up_score.py
+++ hackerrank/find_runner_up_score.py
@@ -1,3 +1,22 @@
+#Task
+
+#Given the participants' score sheet for your University Sports Day,
+#you are required to find the runner-up score.
+#You are given  scores. Store them in a list and find the score of the runner-up.
+
+#Input Format
+
+#The first line contains n. The second line contains an array A[] of integers
+#each separated by a space.
+
+#Constraints
+#2 <= n <= 10
+#-100 <= A[i] <= 100
+
+#Output Format
+
+#Print the runner-up score.
+
 if __name__ == '__main__':
     n = int(input())
     while n < 2 or n >10:
@@ -13,3 +32,4 @@ if __name__ == '__main__':
 
     sorted_arr = list(set(sorted(arr, reverse=True)))
     print(sorted_arr[1])
+
